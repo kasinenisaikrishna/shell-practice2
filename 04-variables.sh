7f5657cb28d526106613aea47208298dc0324d87
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-26 10:16:38 +0530

    shell

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=$1
+person2=$2
+
+echo "$1:: hi ${2}, how r u?"
+echo "${2}:: hello $1 I am fine. how r u doing?"
+echo "$1:: I am doing good ${2}. what's going on?"
+echo "${2}:: I started learning shell script $1"
