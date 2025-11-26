1dc61747f53b241b3bf487843fe390038898b985
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-26 10:42:10 +0530

    shell

---
+++ 06-aray.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+fruits=("apple", "kiwi", "orange")
+
+echo "first fruit is: ${fruits[0]}"
+echo "second fruit is: ${fruits[1]}"
+echo "third fruit is: ${fruits[2]}"
+
+echo "all fruits: ${fruits[@]}"
