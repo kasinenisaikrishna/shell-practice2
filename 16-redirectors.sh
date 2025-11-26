35063a975785966085ab6033525c15e0d9c10848
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-26 17:45:33 +0530

    shell

---
+++ 16-redirectors.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+logs_folder="/var/log/shell-script"
+script_name=$(echo $0 | cut -d "." -f1)
+time_stamp=$(date +%Y-%m-%d-%H-%M-%S)
+log_file="$logs_folder/$script_name-$time_stamp.log"
+mkdir -p $logs_folder
+
+userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+check_root(){
+    if [ $userid -ne 0 ]
+    then
+        echo -e "$R Please run this script with root access $N" &>>$log_file
+        exit 1
+    fi
+}
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R failed $N" &>>$log_file
+        exit 1
+    else
+        echo -e "$2 is...$G success $N" &>>$log_file
+    fi
+}
+
+check_root
