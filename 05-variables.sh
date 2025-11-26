dd9397624cf34f0474d38f8e2e13d6bb4d937a2d
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-26 10:23:28 +0530

    shell

---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "please enter your username::"
+read -s username
+echo "username enter is: $username"
+echo "please enter your username::"
+read -s password
+echo "username enter is: $password"
