c76cfcf6eaad227de2e1ffea211b46d299c21cb7
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-26 14:05:38 +0530

    shell

--- 11-installation.sh
+++ 11-installation.sh
@@ -8,4 +8,12 @@ then
     exit 1
 fi
 
-dnf install git -y
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    echo "git is not installed, going to install it.."
+    dnf install git -y
+else
+    echo "git is already installed, nothing to do.."
+fi
