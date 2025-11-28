14cdff893c67374d9718109b2bc0768a1a2c8474
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-28 08:08:58 +0530

    shell

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e # setting the automatic exit, if we get an error
+set -ex # setting the automatic exit, if we get an error. x is for debug
 echo "hello world success"
 echooo "hello world failure"
 echo "hello world after failure"
